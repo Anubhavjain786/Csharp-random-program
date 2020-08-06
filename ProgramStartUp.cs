4fd28cff252145fbe4f784110c83e608d8333fd5
Anubhav Jain <anubhavj660@gmail.com>
2020-08-06 10:43:45 +0530

    ADD: Innitial Commit

---
+++ ProgramStartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRandomProgram
+{
+    class ProgramStartUp
+    {
+        public static void Main(string[] args)
+        {
+            //FibonacciSeries.code();
+            //PrimeNumber.code();
+            // PalindromeNumber.code();
+            //Factorial.code();
+            //ArmstrongNumber.code();
+            BuiltInDataType.testing();
+        }
+    }
+    public static class BuiltInDataType
+    {
+        public static void testing()
+        {
+            short Short;
+            Short = 10;
+            Console.WriteLine(sizeof(short));
+           
+           
+        }
+    }
+    /// <summary>
+    /// this is sample class 
+    /// </summary>
+    public static class semple
+    {
+
+    }
+}
